82ecc427252e26b523cd8fac7969dee5028d89e9
Sơn Nguyễn <son@sonng.dev>
2023-02-04 00:19:03 +0000

    feat: Allow raw HTML in markdown

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,12 +1,14 @@
 import type { VFileCompatible } from 'vfile';
 import { remark } from 'remark';
 import remarkRehype from 'remark-rehype';
+import rehypeRaw from 'rehype-raw';
 import rehypePrism from '@mapbox/rehype-prism';
 import rehypeStringify from 'rehype-stringify';
 
 export default async function markdownToHtml(markdown: VFileCompatible) {
   const result = await remark()
-    .use(remarkRehype)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
     .use(rehypePrism)
     .use(rehypeStringify)
     .process(markdown);

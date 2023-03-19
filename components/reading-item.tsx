9f6fea7cffea05facff6a7b82c20d52c15f20cdf
Sơn Nguyễn <son@sonng.dev>
2023-03-19 20:10:21 +0000

    feat: Use horizontal layout from sm breakpoint on /readings

--- components/reading-item.tsx
+++ components/reading-item.tsx
@@ -14,7 +14,7 @@ export default function ReadingItem(props: ReadingItemProps) {
   const { thumbnail, title, author, publicationYear, description, url } = props;
 
   return (
-    <div className="flex flex-col lg:flex-row gap-4">
+    <div className="flex flex-col sm:flex-row gap-4">
       <Image
         src={thumbnail}
         alt="ChatGPT With Voice"
